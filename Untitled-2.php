e59477d26308af17b067fd9f28dd164504a8582f
Asha S Madarakhandi <amadarakhand@adobe.com>
2022-06-04 17:28:13 +0530

    2testingftp

---
+++ Untitled-2.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+</body>
+</html>
